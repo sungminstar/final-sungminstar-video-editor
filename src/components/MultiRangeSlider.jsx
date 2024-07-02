22d2fa6c2993a8f7fa5d4bfc80d0271322fe2d5e
sungminstar <sungmin8453@naver.com>
2024-07-02 20:34:23 +0900

    feat: 슬라이더 주석

--- src/components/MultiRangeSlider.jsx
+++ src/components/MultiRangeSlider.jsx
@@ -1,51 +1,42 @@
 import { useCallback, useEffect, useState, useRef } from "react";
 import classnames from "classnames";
 import "./multiRangeSlider.css";
-
+/* 비디오 재생 시간 멀티 슬라이더 0 ~ 100 */
 export default function MultiRangeSlider({ min, max, onChange, disabled }) {
-  // 최소 값과 최대 값 상태 설정
   const [minVal, setMinVal] = useState(min);
   const [maxVal, setMaxVal] = useState(max);
-
-  // 최소 값 및 최대 값의 Ref 설정
   const minValRef = useRef(null);
   const maxValRef = useRef(null);
-
-  // range 요소의 Ref 설정
   const range = useRef(null);
 
-  // 입력 값의 백분율을 계산하는 콜백 함수
+  // 입력 값의 백분율을 계산
   const getPercent = useCallback(
     (value) => Math.round(((value - min) / (max - min)) * 100),
     [min, max]
   );
 
+  // 슬라이더 값 min
   useEffect(() => {
-    // minVal의 백분율 계산
     const minPercent = getPercent(minVal);
-    // maxVal의 백분율 계산
     const maxPercent = getPercent(maxVal);
 
-    // range 요소의 위치 및 너비 조정
     if (range.current) {
       range.current.style.left = `${minPercent}%`;
       range.current.style.width = `${maxPercent - minPercent}%`;
     }
   }, [minVal, maxVal, getPercent]);
 
+  // 슬라이더 값 max
   useEffect(() => {
-    // minValRef의 백분율 계산
     const minPercent = getPercent(+minValRef.current.value);
-    // maxVal의 백분율 계산
     const maxPercent = getPercent(maxVal);
 
-    // range 요소의 너비 조정
     if (range.current) {
       range.current.style.width = `${maxPercent - minPercent}%`;
     }
   }, [maxVal, getPercent]);
 
-  // minVal과 maxVal 값이 변경될 때 onChange 함수 호출
+  // minVal과 maxVal 값 업데이트
   useEffect(() => {
     onChange({ min: minVal, max: maxVal });
   }, [minVal, maxVal, onChange]);
--- src/components/VideoDuration.jsx
+++ src/components/VideoDuration.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { toTimeString } from "../utils/utils";
 
+/* 비디오 재생 시간 구하는 컴포넌트 feat.toTimeString */
 const VideoDuration = ({ selectedDuration, totalDuration }) => {
   const formatTime = (timeInSeconds) => {
     return toTimeString(timeInSeconds);
